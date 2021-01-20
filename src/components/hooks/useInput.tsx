7de73381e12577871579be24769dd093ed4c840e
Dawid <nexog1997@gmail.com>
2021-01-20 23:38:07 +0100

    input custom hook

---
+++ src/components/hooks/useInput.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export const useInput = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue)
+
+  return {
+    value,
+    setValue,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value)
+    },
+  }
+}
