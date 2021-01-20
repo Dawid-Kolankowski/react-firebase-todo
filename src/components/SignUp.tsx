43836887e027eed3c1ed72208f40c0685d84f107
Dawid <nexog1997@gmail.com>
2021-01-20 23:40:12 +0100

    sign up page structure/no css

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import { useInput } from './hooks/useInput'
 
 const SignUp: React.FC = () => {
-  return <div>signup</div>
+  const { value: email, onChange: onChangeEmail } = useInput('')
+  const { value: password, onChange: onChangePassword } = useInput('')
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(email, password)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={onChangeEmail}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChangePassword}
+          />
+        </label>
+        <button type="submit">Sign Up</button>
+        or
+        <button type="button">Sign Up with google</button>
+      </form>
+    </div>
+  )
 }
 
 export default SignUp
