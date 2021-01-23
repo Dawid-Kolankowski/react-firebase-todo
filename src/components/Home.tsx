38690a73fda398e918b595a5effd60745b3447f3
Dawid <nexog1997@gmail.com>
2021-01-23 23:44:57 +0100

    sign out button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import CreateTodoInput from './CreateTodoInput'
 import { TodoProvider } from './providers/TodoProvider'
 import TodoTable from './TodoTable'
+import firebaseApp from '../firebase'
 
 const Home: React.FC = () => {
+  const onSignOut = () => {
+    firebaseApp.auth().signOut()
+  }
+
   return (
     <>
       <TodoProvider>
         <CreateTodoInput />
         <TodoTable />
       </TodoProvider>
+      <button type="button" className="signout" onClick={onSignOut}>
+        Sign Out
+      </button>
     </>
   )
 }
